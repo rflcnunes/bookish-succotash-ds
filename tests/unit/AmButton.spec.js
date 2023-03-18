f30d26967da07cbbb57a9f64187a874392e238f0
rafael <rafaelcnunes@icloud.com>
2023-03-18 20:42:32 -0300

    added testes to AmButton

--- tests/unit/AmButton.spec.js
+++ tests/unit/AmButton.spec.js
@@ -2,11 +2,48 @@ import { shallowMount } from "@vue/test-utils";
 import AmButton from "@/components/atoms/AmButton/AmButton.vue";
 
 describe("AmButton.vue", () => {
-  it("renders props.msg when passed", () => {
+  it("renders props.label when passed", () => {
     const label = "Label Button";
     const wrapper = shallowMount(AmButton, {
       propsData: { label },
     });
     expect(wrapper.text()).toMatch(label);
+  });
+
+  it("renders a button element", () => {
+    const wrapper = shallowMount(AmButton);
+    expect(wrapper.find("button").exists()).toBe(true);
+  });
+
+  it("displays the correct label", () => {
+    const wrapper = shallowMount(AmButton, {
+      propsData: {
+        label: "Test Label",
+      },
+    });
+    expect(wrapper.text()).toContain("Test Label");
+  });
+
+  it("emits a click event when clicked", async () => {
+    const wrapper = shallowMount(AmButton);
+    await wrapper.trigger("click");
+    expect(wrapper.emitted("click")).toBeTruthy();
+  });
+
+  it("applies the correct class based on props", () => {
+    const wrapper = shallowMount(AmButton, {
+      propsData: {
+        size: "small",
+        variant: "outlined",
+      },
+    });
+    expect(wrapper.classes()).toContain("am-button--small");
+    expect(wrapper.classes()).toContain("am-button--outlined");
+  });
+
+  it("applies the clicked class when clicked", async () => {
+    const wrapper = shallowMount(AmButton);
+    await wrapper.trigger("click");
+    expect(wrapper.classes()).toContain("am-button--clicked");
   });
 });
