ccee0d06a6a3ada74cf8e61a1d45dbd99130a7da
rafael <rafaelcnunes@icloud.com>
2023-03-25 15:50:59 -0300

    added typescript

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,43 @@
 module.exports = {
   root: true,
+
   env: {
-    node: true
+    node: true,
   },
-  extends: ["plugin:vue/essential", "eslint:recommended", "plugin:prettier/recommended", "plugin:storybook/recommended"],
+
+  extends: [
+    "plugin:vue/essential",
+    "eslint:recommended",
+    "plugin:prettier/recommended",
+    "plugin:storybook/recommended",
+  ],
+
   parserOptions: {
-    parser: "@babel/eslint-parser"
+    parser: "@typescript-eslint/parser",
   },
+
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off"
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
   },
-  overrides: [{
-    files: ["**/__tests__/*.{j,t}s?(x)", "**/tests/unit/**/*.spec.{j,t}s?(x)"],
-    env: {
-      jest: true
-    }
-  }]
+
+  overrides: [
+    {
+      files: [
+        "**/__tests__/*.{j,t}s?(x)",
+        "**/tests/unit/**/*.spec.{j,t}s?(x)",
+      ],
+      env: {
+        jest: true,
+      },
+    },
+  ],
+
+  extends: [
+    "plugin:vue/essential",
+    "eslint:recommended",
+    "plugin:prettier/recommended",
+    "plugin:storybook/recommended",
+    "@vue/typescript",
+  ],
 };
