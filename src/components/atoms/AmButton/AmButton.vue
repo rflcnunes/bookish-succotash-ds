c67850075a6b549f1567bec561d57b729d331b5b
rafael <rafaelcnunes@icloud.com>
2023-04-06 23:26:21 -0300

    adjustment new variant maxDarkGhost, added in validator

--- src/components/atoms/AmButton/AmButton.vue
+++ src/components/atoms/AmButton/AmButton.vue
@@ -44,6 +44,7 @@ export default {
           "maxPrimary",
           "maxOutlined",
           "maxGhost",
+          "maxDarkGhost",
           "maxSecondary",
           "maxSuccess",
           "maxError",
