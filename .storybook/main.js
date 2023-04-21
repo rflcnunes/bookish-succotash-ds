ea2d6bf69bc0d4f6ab2ea53d09cad93cb31db4e4
rafael <rafaelcnunes@icloud.com>
2023-04-21 03:50:48 -0300

    upgrade storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,19 +1,11 @@
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
-    "@storybook/preset-scss",
-    "@storybook/addon-viewport",
-    "@storybook/addon-measure",
-    "@storybook/addon-outline",
-  ],
-  "framework": "@storybook/vue",
-  "core": {
-    "builder": "@storybook/builder-webpack5"
+  "stories": ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  "addons": ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions", "@storybook/preset-scss", "@storybook/addon-viewport", "@storybook/addon-measure", "@storybook/addon-outline", "@storybook/addon-mdx-gfm"],
+  "framework": {
+    name: "@storybook/vue-webpack5",
+    options: {}
+  },
+  docs: {
+    autodocs: true
   }
-}
+};
