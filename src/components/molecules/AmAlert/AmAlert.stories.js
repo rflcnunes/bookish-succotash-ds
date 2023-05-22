9b073569985c2e871946b0e37bb70d8dc8b1127d
rafael <rafaelcnunes@icloud.com>
2023-05-22 01:49:38 -0300

    clean components

--- src/components/molecules/AmAlert/AmAlert.stories.js
+++ src/components/molecules/AmAlert/AmAlert.stories.js
@@ -1,74 +1,74 @@
-import AmAlert from "./AmAlert.vue";
+// import AmAlert from "./AmAlert.vue";
 
-export default {
-  title: "Molecules/AmAlert",
-  component: AmAlert,
-  argTypes: {
-    variation: {
-      control: {
-        type: "select",
-        options: ["warning", "error", "success", "info", "helper"],
-      },
-    },
-    title: {
-      control: "text",
-    },
-    text: {
-      control: "text",
-    },
-    link: {
-      control: "text",
-    },
-  },
-};
+// export default {
+//   title: "Molecules/AmAlert",
+//   component: AmAlert,
+//   argTypes: {
+//     variation: {
+//       control: {
+//         type: "select",
+//         options: ["warning", "error", "success", "info", "helper"],
+//       },
+//     },
+//     title: {
+//       control: "text",
+//     },
+//     text: {
+//       control: "text",
+//     },
+//     link: {
+//       control: "text",
+//     },
+//   },
+// };
 
-const Template = (args, { argTypes }) => ({
-  props: Object.keys(argTypes),
-  components: { AmAlert },
-  setup() {
-    return { args };
-  },
-  template: '<am-alert v-bind="args" />',
-});
+// const Template = (args, { argTypes }) => ({
+//   props: Object.keys(argTypes),
+//   components: { AmAlert },
+//   setup() {
+//     return { args };
+//   },
+//   template: '<am-alert v-bind="args" />',
+// });
 
-export const Default = Template.bind({});
-Default.args = {
-  variation: "success",
-  title: "Success",
-  text: "This is a success message!",
-};
+// export const Default = Template.bind({});
+// Default.args = {
+//   variation: "success",
+//   title: "Success",
+//   text: "This is a success message!",
+// };
 
-export const Warning = Template.bind({});
-Warning.args = {
-  variation: "warning",
-  title: "This is a warning",
-  text: "Be careful!",
-};
+// export const Warning = Template.bind({});
+// Warning.args = {
+//   variation: "warning",
+//   title: "This is a warning",
+//   text: "Be careful!",
+// };
 
-export const Error = Template.bind({});
-Error.args = {
-  variation: "error",
-  title: "Error",
-  text: "An error has occurred.",
-};
+// export const Error = Template.bind({});
+// Error.args = {
+//   variation: "error",
+//   title: "Error",
+//   text: "An error has occurred.",
+// };
 
-export const Success = Template.bind({});
-Success.args = {
-  variation: "success",
-  title: "Success!",
-  text: "Your action was successful.",
-};
+// export const Success = Template.bind({});
+// Success.args = {
+//   variation: "success",
+//   title: "Success!",
+//   text: "Your action was successful.",
+// };
 
-export const Info = Template.bind({});
-Info.args = {
-  variation: "info",
-  title: "Information",
-  text: "This is some important information.",
-};
+// export const Info = Template.bind({});
+// Info.args = {
+//   variation: "info",
+//   title: "Information",
+//   text: "This is some important information.",
+// };
 
-export const Helper = Template.bind({});
-Helper.args = {
-  variation: "helper",
-  title: "Helper",
-  text: "This is some helpful text.",
-};
+// export const Helper = Template.bind({});
+// Helper.args = {
+//   variation: "helper",
+//   title: "Helper",
+//   text: "This is some helpful text.",
+// };
